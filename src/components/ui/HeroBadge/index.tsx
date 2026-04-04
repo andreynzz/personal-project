82de2f185034b7048d23411949a020edc51f23b6
drey-dev <andrey.dhorapirola@gmail.com>
2026-04-04 13:09:02 -0300

    refactor: reorganize sections and ui component structure

--- src/components/ui/HeroBadge/index.tsx
+++ src/components/ui/HeroBadge/index.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { heroBadges } from "../data/heroBadges";
+import { heroBadges } from "../../../data/heroBadges";
 import styles from "./HeroBadge.module.scss";
 
 export default function HeroBadge() {
