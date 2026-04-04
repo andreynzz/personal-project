72014258634387dc6e9bc2b78cd583078f92fe50
drey-dev <andrey.dhorapirola@gmail.com>
2026-04-04 13:45:20 -0300

    refactor: extract hero ticker items into data module

---
+++ src/data/heroTickerItems.ts
@@ -0,0 +1,8 @@
+export const heroTickerItems = [
+  "Treinamento personalizado",
+  "Emagrecimento real",
+  "Hipertrofia com método",
+  "Acompanhamento 1:1",
+  "Resultados em 12 semanas",
+  "+120 alunos"
+];
